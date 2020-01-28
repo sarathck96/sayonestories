e06300d902a950c4ad887aa69ffbb536408f0265
megha <megha.sayonetech@gmail.com>
2020-01-28 16:13:04 +0530

    Deleted temp folders

--- project/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/sayone/Desktop/project/Gitfolder/sayonestories/project/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
